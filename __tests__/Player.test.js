ab44f8251b2aa66278c3f2d43a824a7262ca2c6e
mpeoples22 <mpeoples2020@gmail.com>
2022-01-12 22:29:34 -0600

    arrayTest

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -3,6 +3,9 @@ const Player = require('../lib/Player');
 
 
 
+const Potion = require('../lib/Potion');
+jest.mock('../lib/Potion');
+console.log(new Potion());
 
 /*
 
@@ -22,4 +25,5 @@ test('creates a player object', () => {
   expect(player.health).toEqual(expect.any(Number));
   expect(player.strength).toEqual(expect.any(Number));
   expect(player.agility).toEqual(expect.any(Number));
+  expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
 });
--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -3,6 +3,7 @@ const Potion = require('../lib/Potion.js');
 
 //...
 
+
 test('creates a health potion object', () => {
     const potion = new Potion('health');
     expect(potion.name).toBe('health');
